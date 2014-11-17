6d0ea8dd337becb5ef2366770643d531486dca38
mala23 <mail@mala23.me>
2014-11-17 16:51:08 +0000

    use variables

--- Introductions.java
+++ Introductions.java
@@ -4,6 +4,11 @@ public class Introductions {
 
   public static void main(String[] args) {
     Console console = System.console();
-    console.printf("Hello, my name is Colin");
+    String firstName = "Colin";
+    // Variable names in Java are defined in camel case
+    console.printf("Hello, my name is %s\n", firstName);
+    // printf or print formated allows escape sequences and variables in
+    // strings that are then formatted before output
+    console.printf("%s is learning to write Java\n", firstName);
   }
 }
